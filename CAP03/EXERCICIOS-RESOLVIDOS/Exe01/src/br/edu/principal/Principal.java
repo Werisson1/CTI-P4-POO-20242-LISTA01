f3339686dcca48d6b1386c0cfcc32f8c9722cf65
Werisson1 <f.werisson@aluno.ifce.edu.br>
2024-10-29 16:56:36 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/Exe01/src/br/edu/principal/Principal.java
@@ -0,0 +1,19 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int n1 , n2 , n3 , n4 , soma;
+		
+		n1 = 1;
+		n2 = 2;
+		n3 = 3;
+		n4 = 4;
+		
+		soma = n1 + n2 + n3 + n4;
+		
+		System.out.println("a soma dos números 1, 2, 3, 4 é: " + soma);
+
+	}
+
+}
