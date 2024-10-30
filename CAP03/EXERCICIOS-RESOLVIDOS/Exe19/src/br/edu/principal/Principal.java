8d6b7bbd39c9221ab6c8844aa614f22d6e912a72
Werisson1 <f.werisson@aluno.ifce.edu.br>
2024-10-30 09:28:39 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/Exe19/src/br/edu/principal/Principal.java
@@ -0,0 +1,17 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double a_degrau, a_usuario, qtd_degraus;
+		
+		a_usuario = 10;
+		a_degrau = 30;
+		
+		qtd_degraus = a_usuario / a_degrau;
+		
+		System.out.println(qtd_degraus);
+
+	}
+
+}
