8fc970557c42c74fafb2cdde6e4a438a0fe2f148
Werisson1 <f.werisson@aluno.ifce.edu.br>
2024-10-30 09:29:34 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/Exe21/src/br/edu/principal/Principal.java
@@ -0,0 +1,18 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		 double X, Y, Z;
+		 
+		 Z = 7;
+		 X = 1;
+		 
+		 Y = Math.pow(Z,2) - Math.pow(X,2);
+		 Y = Math.pow(Y, 1/2);
+		 
+		 System.out.println(Y);
+
+	}
+
+}
