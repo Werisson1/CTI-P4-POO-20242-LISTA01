e65b6eafa3e4237470b0cca633d1a682f57181e8
Werisson1 <f.werisson@aluno.ifce.edu.br>
2024-10-30 09:31:27 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/Exe25/src/br/edu/principal/Principal.java
@@ -0,0 +1,15 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+	double custo, convite, qtd;
+	custo = 20;
+	convite = 5;
+	qtd  = custo/convite;
+	
+	System.out.println(qtd);
+
+	}
+
+}
