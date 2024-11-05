27a3406e2d9ca075e45e6742aed2826f1541339b
Wérisson <f.werisson@aluno.ifce.edu.br>
2024-11-05 13:07:03 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/Exe10/src/br/edu/principal/Principal.java
@@ -0,0 +1,15 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double area, raio, raio2;
+		raio = 3;
+		raio2 = raio * raio;
+		area = 3.1415 * raio2;
+		
+		System.out.println("a área do círculo é a igual: " + area);
+
+	}
+
+}
