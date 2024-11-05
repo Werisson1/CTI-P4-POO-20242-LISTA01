b9b8d4716a2ca01650775b5d00c61bc152aa87bd
Wérisson <f.werisson@aluno.ifce.edu.br>
2024-11-05 13:07:49 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/Exe12/src/br/edu/principal/Principal.java
@@ -0,0 +1,17 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double num1, num2, r1, r2;
+		num1 = 7;
+		num2 = 4;
+		r1 = Math.pow(num1 , num2);
+		r2 = Math.pow(num2 , num1);
+		
+		System.out.println(r1);
+		System.out.println(r2);
+
+	}
+
+}
