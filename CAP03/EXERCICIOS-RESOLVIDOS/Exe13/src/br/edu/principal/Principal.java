a2e7c9aadb65aced9d80d7c360f94f5eab342477
Wérisson <f.werisson@aluno.ifce.edu.br>
2024-11-05 13:08:10 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/Exe13/src/br/edu/principal/Principal.java
@@ -0,0 +1,18 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double pes, polegadas, jardas, milhas;
+		pes = 15;
+		polegadas = pes * 12;
+		jardas = pes / 3;
+		milhas = jardas / 1760;
+		
+		System.out.println(polegadas);
+		System.out.println(jardas);
+		System.out.println(milhas);
+
+	}
+
+}
