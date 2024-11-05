b94fc56bf048263189aab893211e0dfb88ce84d9
Wérisson <f.werisson@aluno.ifce.edu.br>
2024-11-05 13:02:29 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/Exe04/src/br/edu/principal/Principal.java
@@ -0,0 +1,16 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int sal, novosal;
+		
+		sal = 7;
+		
+		novosal = sal + sal * 25/100;
+		
+		System.out.println("o novo salário é igual a: " +novosal);
+
+	}
+
+}
