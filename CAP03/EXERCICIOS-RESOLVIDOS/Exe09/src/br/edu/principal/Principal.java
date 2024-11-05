7732c26094e4195dbbe690c73d5cf78c69d02c82
Wérisson <f.werisson@aluno.ifce.edu.br>
2024-11-05 13:06:31 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/Exe09/src/br/edu/principal/Principal.java
@@ -0,0 +1,15 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int base, altura, area;
+		
+		base = 34;
+		altura = 87;
+		area = (base * altura) /2 ;
+		
+		System.out.println("a área do triangulo é igual a: ");
+	}
+
+}
